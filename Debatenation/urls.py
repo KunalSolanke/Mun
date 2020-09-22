8258744d86e5a67423ce5c30beab250efd2edf40
VolDemorT-acer <58597818+VolDemorT-acer@users.noreply.github.com>
2020-09-22 10:30:47 +0530

    final touches

--- Debatenation/urls.py
+++ Debatenation/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.views.generic import RedirectView
 
 app_name = 'core'
@@ -10,7 +10,8 @@ urlpatterns = [
    
     path('accounts/',include('accounts.urls')),
     path('chits/',include('chits.urls')),
-     path('',RedirectView.as_view(url='/accounts/login')),
+    path('',RedirectView.as_view(url='/accounts/login')),
+    path("favicon.ico/",RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
 urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
